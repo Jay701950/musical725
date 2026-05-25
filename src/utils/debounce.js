cb5f67b5030f2fbd968edd0b70675185eb4ea41f
Jay701950 <jay7252020@naver.com>
2026-05-25 17:13:48 +0900

    Delete src/utils directory

--- src/utils/debounce.js
+++
@@ -1,8 +0,0 @@
-export function debounce(callback, delay) {
-  let timer;
-
-  return (...args) => {
-    window.clearTimeout(timer);
-    timer = window.setTimeout(() => callback(...args), delay);
-  };
-}
